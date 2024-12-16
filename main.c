b0da39f707159ab3d55a9ac34ca28c79ecdaa048
José Quadrado <joseqcorreia@gmail.com>
2024-12-16 19:05:10 +0000

    Add schrift and text alpha blending.

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include "shm.h"
 #include "wayland-client-protocol.h"
+#include <schrift.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
@@ -11,6 +12,7 @@ typedef struct app_state {
     struct wl_shm* shm;
     struct xdg_wm_base* xdg_base;
     struct wl_surface* surface;
+    SFT sft;
 } app_state;
 
 void registry_handle_global(void* data, struct wl_registry* registry,
@@ -37,6 +39,13 @@ void registry_handle_global_remove(void* data, struct wl_registry* registry,
 {
     /* printf("Removed, name: %d\n", name); */
 }
+typedef struct pixel {
+    unsigned char blue;
+    unsigned char green;
+    unsigned char red;
+    unsigned char alpha;
+} pixel;
+
 static void xdg_surface_configure(void* data, struct xdg_surface* xdg_surface,
     uint32_t serial)
 {
@@ -56,20 +65,82 @@ static void xdg_surface_configure(void* data, struct xdg_surface* xdg_surface,
     int offset = index * height * stride;
 
     struct wl_buffer* buffer = wl_shm_pool_create_buffer(
-        pool, offset, width, height, stride, WL_SHM_FORMAT_XRGB8888);
+        pool, offset, width, height, stride, WL_SHM_FORMAT_ARGB8888);
 
     // This should work by referencing uint8 directly i believe.
     uint32_t* pixels = (uint32_t*)&pool_data[offset];
 
+    SFT_Glyph glyph;
+    SFT_GMetrics metrics;
+    sft_lookup(&state->sft, 'g', &glyph);
+    printf("Glyph ID: %d\n", glyph);
+
+    sft_gmetrics(&state->sft, glyph, &metrics);
+    SFT_Image img = {
+        .width = (metrics.minWidth + 3) & ~3,
+        .height = metrics.minHeight,
+    };
+
+    char gp[img.width * img.height];
+    img.pixels = gp;
+
+    sft_render(&state->sft, glyph, img);
+
+    int text_x = 100;
+    int text_y = 100;
+
+    /*memset(pixels, 0, width * height * sizeof(uint32_t));*/
+
+    // Clear the surface (can't use the memset way for the life of me)
     for (int y = 0; y < height; y++) {
         for (int x = 0; x < width; x++) {
-            if (y > 100 && y < 300) {
-                pixels[y * width + x] = 0xFFFFFFFF;
+            pixel* px = (pixel*)(pool_data + y * stride + x * 4);
+            px->alpha = 255;
+            px->red = 0;
+            px->green = 0;
+            px->blue = 0;
+        }
+    }
+
+    for (int y = 0; y < img.height; y++) {
+        for (int x = 0; x < img.width; x++) {
+            unsigned char val = gp[y * img.width + x];
+            printf("%d\n", val);
+            if (val != 0) {
+                pixel* px = (pixel*)(pool_data + (text_y + y) * stride + (text_x + x) * 4);
+
+                unsigned char alpha = (val + 128) / 255;
+
+                px->red = (255 * val + px->red * px->alpha * (1 - alpha)) / px->alpha;
+                px->green = (255 * val + px->green * px->alpha * (1 - alpha)) / px->alpha;
+                px->blue = (255 * val + px->blue * px->alpha * (1 - alpha)) / px->alpha;
+                px->alpha = val + px->alpha * (1 - alpha);
             }
         }
     }
 
-    /*memset(pixels, 50, width * height * 4);*/
+    text_x += 100;
+    for (int y = 0; y < img.height; y++) {
+        for (int x = 0; x < img.width; x++) {
+            unsigned char val = gp[y * img.width + x];
+            printf("%d\n", val);
+            if (val != 0) {
+                if (y > img.height / 2) {
+                    printf("cenas\n");
+                    val = 100;
+
+                } else {
+                    val = 254;
+                }
+
+                pixel* px = (pixel*)(pool_data + (text_y + y) * stride + (text_x + x) * 4);
+                px->alpha = val;
+                px->red = 100;
+                px->green = 100;
+                px->blue = 100;
+            }
+        }
+    }
 
     wl_surface_attach(state->surface, buffer, 0, 0);
     wl_surface_damage(state->surface, 0, 0, UINT32_MAX, UINT32_MAX);
@@ -80,9 +151,23 @@ static const struct xdg_surface_listener xdg_surface_listener = {
     .configure = xdg_surface_configure,
 };
 
+SFT load_font()
+{
+    int s = 2;
+    SFT sft = {
+        .xScale = 64 * s,
+        .yScale = 64 * s,
+        .flags = SFT_DOWNWARD_Y,
+    };
+    sft.font = sft_loadfile("/usr/share/fonts/TTF/JetBrainsMonoNLNerdFont-Regular.ttf");
+    return sft;
+}
+
 int main(int argc, char* argv[])
 {
     app_state state;
+
+    state.sft = load_font();
 
     struct wl_display* display = wl_display_connect(NULL);
     if (!display) {
