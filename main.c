14553e2348b4bd077bcc125a6c0bf677899d3486
José Quadrado <joseqcorreia@gmail.com>
2024-12-17 01:50:02 +0000

    Next stop: working clock.

--- main.c
+++ main.c
@@ -16,6 +16,7 @@ typedef struct app_state {
     struct xdg_wm_base* xdg_base;
     struct wl_surface* surface;
     struct zwlr_layer_shell_v1* layer_shell;
+
     SFT sft;
 } app_state;
 
@@ -50,13 +51,14 @@ void registry_handle_global(void* data, struct wl_registry* registry,
 }
 
 static void layer_configure(void* data, struct zwlr_layer_surface_v1* layer_surface,
-    uint32_t serial)
+    uint32_t serial, uint32_t width, uint32_t height)
 {
+    printf("Layer configure\n");
     struct app_state* state = data;
     zwlr_layer_surface_v1_ack_configure(layer_surface, serial);
     /* xdg_surface_ack_configure(xdg_surface, serial); */
 
-    const int width = 800, height = 600;
+    /* const int width = 800, height = 600; */
     const int stride = width * 4;
     const int shm_pool_size = height * stride * 2;
 
@@ -71,75 +73,63 @@ static void layer_configure(void* data, struct zwlr_layer_surface_v1* layer_surf
     struct wl_buffer* buffer = wl_shm_pool_create_buffer(
         pool, offset, width, height, stride, WL_SHM_FORMAT_ARGB8888);
 
-    // This should work by referencing uint8 directly i believe.
     /* uint32_t* pixels = (uint32_t*)&pool_data[offset]; */
-
-    SFT_Glyph glyph;
-    SFT_GMetrics metrics;
-    sft_lookup(&state->sft, 'g', &glyph);
-    /* printf("Glyph ID: %ld\n", glyph); */
-
-    sft_gmetrics(&state->sft, glyph, &metrics);
-    SFT_Image img = {
-        .width = (metrics.minWidth + 3) & ~3,
-        .height = metrics.minHeight,
-    };
-
-    char gp[img.width * img.height];
-    img.pixels = gp;
-
-    sft_render(&state->sft, glyph, img);
-
-    int text_x = 100;
-    int text_y = 100;
-
-    /*memset(pixels, 0, width * height * sizeof(uint32_t));*/
 
     // Clear the surface (can't use the memset way for the life of me)
     for (int y = 0; y < height; y++) {
         for (int x = 0; x < width; x++) {
             pixel* px = (pixel*)(pool_data + y * stride + x * 4);
-            px->alpha = 255;
+            px->alpha = 0;
             px->red = 0;
             px->green = 0;
             px->blue = 0;
         }
     }
 
-    for (int y = 0; y < img.height; y++) {
-        for (int x = 0; x < img.width; x++) {
-            unsigned char val = gp[y * img.width + x];
-            /* printf("%d\n", val); */
-            if (val != 0) {
-                pixel* px = (pixel*)(pool_data + (text_y + y) * stride + (text_x + x) * 4);
+    int text_x = 10;
+    int text_y = 50;
 
-                unsigned char alpha = (val + 128) / 255;
+    char* text = "Jose";
+    for (int i = 0; i < strlen(text); i++) {
+        SFT_Glyph glyph;
+        SFT_GMetrics metrics;
+        sft_lookup(&state->sft, text[i], &glyph);
+        /* printf("Glyph ID: %ld\n", glyph); */
 
-                px->red = (255 * val + px->red * px->alpha * (1 - alpha)) / px->alpha;
-                px->green = (255 * val + px->green * px->alpha * (1 - alpha)) / px->alpha;
-                px->blue = (255 * val + px->blue * px->alpha * (1 - alpha)) / px->alpha;
-                px->alpha = val + px->alpha * (1 - alpha);
-            }
-        }
-    }
+        sft_gmetrics(&state->sft, glyph, &metrics);
+        text_x += metrics.advanceWidth;
 
-    text_x += 100;
-    for (int y = 0; y < img.height; y++) {
-        for (int x = 0; x < img.width; x++) {
-            unsigned char val = gp[y * img.width + x];
-            if (val != 0) {
-                if (y > img.height / 2) {
-                    val = 100;
+        SFT_Image img = {
+            .width = metrics.minWidth,
+            .height = metrics.minHeight,
+        };
 
-                } else {
-                    val = 254;
+        char gp[img.width * img.height];
+        img.pixels = gp;
+
+        sft_render(&state->sft, glyph, img);
+
+        /*memset(pixels, 0, width * height * sizeof(uint32_t));*/
+
+        for (int y = 0; y < img.height; y++) {
+            for (int x = 0; x < img.width; x++) {
+                unsigned char val = gp[y * img.width + x];
+                /* printf("%d\n", val); */
+                if (val != 0) {
+                    pixel* px = (pixel*)(pool_data + (text_y + y + metrics.yOffset) * stride + (text_x + x) * 4);
+
+                    unsigned char alpha = (val + 128) / 255;
+
+                    /* px->red = (0 * val + px->red * px->alpha * (1 - alpha)) / px->alpha; */
+                    /* px->green = (0 * val + px->green * px->alpha * (1 - alpha)) / px->alpha; */
+                    /* px->blue = (255 * val + px->blue * px->alpha * (1 - alpha)) / px->alpha; */
+                    /* px->alpha = alpha + px->alpha * (1 - alpha); */
+
+                    px->red = 0;
+                    px->green = 0;
+                    px->blue = 255;
+                    px->alpha = 255;
                 }
-
-                pixel* px = (pixel*)(pool_data + (text_y + y) * stride + (text_x + x) * 4);
-                px->alpha = val;
-                px->red = 100;
-                px->green = 100;
-                px->blue = 100;
             }
         }
     }
@@ -153,109 +143,6 @@ void registry_handle_global_remove(void* data, struct wl_registry* registry,
 {
     /* printf("Removed, name: %d\n", name); */
 }
-
-static void xdg_surface_configure(void* data, struct xdg_surface* xdg_surface,
-    uint32_t serial)
-{
-    struct app_state* state = data;
-    xdg_surface_ack_configure(xdg_surface, serial);
-
-    const int width = 800, height = 600;
-    const int stride = width * 4;
-    const int shm_pool_size = height * stride * 2;
-
-    int fd = allocate_shm_file(shm_pool_size);
-    uint8_t* pool_data = mmap(NULL, shm_pool_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-
-    struct wl_shm_pool* pool = wl_shm_create_pool(state->shm, fd, shm_pool_size);
-
-    int index = 0;
-    int offset = index * height * stride;
-
-    struct wl_buffer* buffer = wl_shm_pool_create_buffer(
-        pool, offset, width, height, stride, WL_SHM_FORMAT_ARGB8888);
-
-    // This should work by referencing uint8 directly i believe.
-    /* uint32_t* pixels = (uint32_t*)&pool_data[offset]; */
-
-    SFT_Glyph glyph;
-    SFT_GMetrics metrics;
-    sft_lookup(&state->sft, 'g', &glyph);
-    /* printf("Glyph ID: %ld\n", glyph); */
-
-    sft_gmetrics(&state->sft, glyph, &metrics);
-    SFT_Image img = {
-        .width = (metrics.minWidth + 3) & ~3,
-        .height = metrics.minHeight,
-    };
-
-    char gp[img.width * img.height];
-    img.pixels = gp;
-
-    sft_render(&state->sft, glyph, img);
-
-    int text_x = 100;
-    int text_y = 100;
-
-    /*memset(pixels, 0, width * height * sizeof(uint32_t));*/
-
-    // Clear the surface (can't use the memset way for the life of me)
-    for (int y = 0; y < height; y++) {
-        for (int x = 0; x < width; x++) {
-            pixel* px = (pixel*)(pool_data + y * stride + x * 4);
-            px->alpha = 255;
-            px->red = 0;
-            px->green = 0;
-            px->blue = 0;
-        }
-    }
-
-    for (int y = 0; y < img.height; y++) {
-        for (int x = 0; x < img.width; x++) {
-            unsigned char val = gp[y * img.width + x];
-            /* printf("%d\n", val); */
-            if (val != 0) {
-                pixel* px = (pixel*)(pool_data + (text_y + y) * stride + (text_x + x) * 4);
-
-                unsigned char alpha = (val + 128) / 255;
-
-                px->red = (255 * val + px->red * px->alpha * (1 - alpha)) / px->alpha;
-                px->green = (255 * val + px->green * px->alpha * (1 - alpha)) / px->alpha;
-                px->blue = (255 * val + px->blue * px->alpha * (1 - alpha)) / px->alpha;
-                px->alpha = val + px->alpha * (1 - alpha);
-            }
-        }
-    }
-
-    text_x += 100;
-    for (int y = 0; y < img.height; y++) {
-        for (int x = 0; x < img.width; x++) {
-            unsigned char val = gp[y * img.width + x];
-            if (val != 0) {
-                if (y > img.height / 2) {
-                    val = 100;
-
-                } else {
-                    val = 254;
-                }
-
-                pixel* px = (pixel*)(pool_data + (text_y + y) * stride + (text_x + x) * 4);
-                px->alpha = val;
-                px->red = 100;
-                px->green = 100;
-                px->blue = 100;
-            }
-        }
-    }
-
-    wl_surface_attach(state->surface, buffer, 0, 0);
-    wl_surface_damage(state->surface, 0, 0, UINT32_MAX, UINT32_MAX);
-    wl_surface_commit(state->surface);
-}
-
-static const struct xdg_surface_listener xdg_surface_listener = {
-    .configure = xdg_surface_configure,
-};
 
 static const struct zwlr_layer_surface_v1_listener layer_listener = {
     .configure = layer_configure,
@@ -271,8 +158,8 @@ SFT load_font(char* name)
     printf("%s\n", res);
     int s = 2;
     SFT sft = {
-        .xScale = 64 * s,
-        .yScale = 64 * s,
+        .xScale = 32 * s,
+        .yScale = 32 * s,
         .flags = SFT_DOWNWARD_Y,
     };
     sft.font = sft_loadfile(name);
@@ -305,17 +192,19 @@ int main(int argc, char* argv[])
 
     // Only after first round trip state.compositor is set
     state.surface = wl_compositor_create_surface(state.compositor);
+
+    // Turn surface into layer
     struct zwlr_layer_surface_v1* layer = zwlr_layer_shell_v1_get_layer_surface(state.layer_shell, state.surface, NULL, 3, "test");
     zwlr_layer_surface_v1_add_listener(layer, &layer_listener, &state);
 
+    // xdg_surface stuff, disregard
     /* struct xdg_surface* xdg_surface = xdg_wm_base_get_xdg_surface(state.xdg_base, state.surface); */
     /* xdg_surface_add_listener(xdg_surface, &xdg_surface_listener, &state); */
-
     /* struct xdg_toplevel* toplevel = xdg_surface_get_toplevel(xdg_surface); */
-
     /* xdg_toplevel_set_title(toplevel, "Wayland test"); */
-    zwlr_layer_surface_v1_set_size(layer, 800, 600);
-    zwlr_layer_surface_v1_set_anchor(layer, 2);
+
+    zwlr_layer_surface_v1_set_size(layer, 320, 100);
+    zwlr_layer_surface_v1_set_anchor(layer, ZWLR_LAYER_SURFACE_V1_ANCHOR_BOTTOM | ZWLR_LAYER_SURFACE_V1_ANCHOR_RIGHT);
     wl_surface_commit(state.surface);
 
     // Loop
---
+++ xdg.c
@@ -0,0 +1,105 @@
+#include <xdg-shell.h>
+
+// Mess of a code for documentation purposes
+static void xdg_surface_configure(void* data, struct xdg_surface* xdg_surface,
+    uint32_t serial)
+{
+    struct app_state* state = data;
+    xdg_surface_ack_configure(xdg_surface, serial);
+
+    const int width = 800, height = 600;
+    const int stride = width * 4;
+    const int shm_pool_size = height * stride * 2;
+
+    int fd = allocate_shm_file(shm_pool_size);
+    uint8_t* pool_data = mmap(NULL, shm_pool_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+
+    struct wl_shm_pool* pool = wl_shm_create_pool(state->shm, fd, shm_pool_size);
+
+    int index = 0;
+    int offset = index * height * stride;
+
+    struct wl_buffer* buffer = wl_shm_pool_create_buffer(
+        pool, offset, width, height, stride, WL_SHM_FORMAT_ARGB8888);
+
+    // This should work by referencing uint8 directly i believe.
+    /* uint32_t* pixels = (uint32_t*)&pool_data[offset]; */
+
+    SFT_Glyph glyph;
+    SFT_GMetrics metrics;
+    sft_lookup(&state->sft, 'g', &glyph);
+    /* printf("Glyph ID: %ld\n", glyph); */
+
+    sft_gmetrics(&state->sft, glyph, &metrics);
+    SFT_Image img = {
+        .width = (metrics.minWidth + 3) & ~3,
+        .height = metrics.minHeight,
+    };
+
+    char gp[img.width * img.height];
+    img.pixels = gp;
+
+    sft_render(&state->sft, glyph, img);
+
+    int text_x = 100;
+    int text_y = 100;
+
+    /*memset(pixels, 0, width * height * sizeof(uint32_t));*/
+
+    // Clear the surface (can't use the memset way for the life of me)
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            pixel* px = (pixel*)(pool_data + y * stride + x * 4);
+            px->alpha = 255;
+            px->red = 0;
+            px->green = 0;
+            px->blue = 0;
+        }
+    }
+
+    for (int y = 0; y < img.height; y++) {
+        for (int x = 0; x < img.width; x++) {
+            unsigned char val = gp[y * img.width + x];
+            /* printf("%d\n", val); */
+            if (val != 0) {
+                pixel* px = (pixel*)(pool_data + (text_y + y) * stride + (text_x + x) * 4);
+
+                unsigned char alpha = (val + 128) / 255;
+
+                px->red = (255 * val + px->red * px->alpha * (1 - alpha)) / px->alpha;
+                px->green = (255 * val + px->green * px->alpha * (1 - alpha)) / px->alpha;
+                px->blue = (255 * val + px->blue * px->alpha * (1 - alpha)) / px->alpha;
+                px->alpha = val + px->alpha * (1 - alpha);
+            }
+        }
+    }
+
+    text_x += 100;
+    for (int y = 0; y < img.height; y++) {
+        for (int x = 0; x < img.width; x++) {
+            unsigned char val = gp[y * img.width + x];
+            if (val != 0) {
+                if (y > img.height / 2) {
+                    val = 100;
+
+                } else {
+                    val = 254;
+                }
+
+                pixel* px = (pixel*)(pool_data + (text_y + y) * stride + (text_x + x) * 4);
+                px->alpha = val;
+                px->red = 100;
+                px->green = 100;
+                px->blue = 100;
+            }
+        }
+    }
+
+    wl_surface_attach(state->surface, buffer, 0, 0);
+    wl_surface_damage(state->surface, 0, 0, UINT32_MAX, UINT32_MAX);
+    wl_surface_commit(state->surface);
+}
+
+static const struct xdg_surface_listener xdg_surface_listener = {
+    .configure = xdg_surface_configure,
+};
